5defcc61bbf2d54e5a51cb4831e2184656f260cd
Andrey_Beloboky <andrei.beloboky@yandex.ru>
2026-04-27 14:55:37 +0300

    add ddl.sql

---
+++ src/main/resources/ddl.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tasks (
+                       id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                       title VARCHAR(100) NOT NULL UNIQUE,
+                       description TEXT NOT NULL,
+                       status VARCHAR NOT NULL DEFAULT 'PENDING',
+                       duedate DATE NOT NULL,
+                       CONSTRAINT title_length_check CHECK (char_length(title) BETWEEN 5 AND 100),
+                       CONSTRAINT description_length_check CHECK (char_length(description) <= 500),
+                       CONSTRAINT duedate_in_future CHECK (duedate > current_date)
+);
