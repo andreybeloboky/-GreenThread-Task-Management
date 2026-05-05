7ea8a75ff3e58113ec49688e5cb8393502119704
Andrey_Beloboky <andrei.beloboky@yandex.ru>
2026-05-05 17:11:12 +0300

    delete optional in pom.xml. add new subtasks table in ddl.sql

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,10 +1,24 @@
-CREATE TABLE tasks (
-                       id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-                       title VARCHAR NOT NULL UNIQUE,
-                       description TEXT NOT NULL,
-                       status VARCHAR NOT NULL DEFAULT 'PENDING',
-                       duedate DATE NOT NULL,
-                       CONSTRAINT title_length_check CHECK (char_length(title) BETWEEN 5 AND 100),
-                       CONSTRAINT description_length_check CHECK (char_length(description) <= 500),
-                       CONSTRAINT duedate_in_future CHECK (duedate > current_date)
+CREATE TABLE tasks
+(
+    id          INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title       VARCHAR NOT NULL UNIQUE,
+    description TEXT    NOT NULL,
+    status      VARCHAR NOT NULL DEFAULT 'PENDING',
+    duedate     DATE    NOT NULL,
+    CONSTRAINT title_length_check CHECK (char_length(title) BETWEEN 5 AND 100),
+    CONSTRAINT description_length_check CHECK (char_length(description) <= 500),
+    CONSTRAINT duedate_in_future CHECK (duedate > current_date)
 );
+
+CREATE TABLE subtasks
+(
+    id         INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    task_id    INT     NOT NULL,
+    title      VARCHAR NOT NULL UNIQUE,
+    completed  BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP        DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_task
+        FOREIGN KEY (task_id)
+            REFERENCES tasks (id)
+            ON DELETE CASCADE
+)
