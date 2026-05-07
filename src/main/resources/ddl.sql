fe44962400459a269157eea6074ca3daaa0a2fca
Andrey_Beloboky <andrei.beloboky@yandex.ru>
2026-05-07 18:15:04 +0300

    update ddl.sql

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -4,7 +4,7 @@ CREATE TABLE tasks
     title       VARCHAR NOT NULL UNIQUE,
     description TEXT    NOT NULL,
     status      VARCHAR NOT NULL DEFAULT 'PENDING',
-    duedate     DATE    NOT NULL,
+    duedate     TIMESTAMP WITH TIME ZONE NOT NULL,
     CONSTRAINT title_length_check CHECK (char_length(title) BETWEEN 5 AND 100),
     CONSTRAINT description_length_check CHECK (char_length(description) <= 500),
     CONSTRAINT duedate_in_future CHECK (duedate > current_date)
@@ -16,7 +16,7 @@ CREATE TABLE subtasks
     task_id    INT     NOT NULL,
     title      VARCHAR NOT NULL UNIQUE,
     completed  BOOLEAN NOT NULL DEFAULT FALSE,
-    created_at TIMESTAMP        DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     CONSTRAINT fk_task
         FOREIGN KEY (task_id)
             REFERENCES tasks (id)
