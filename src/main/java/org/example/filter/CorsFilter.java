8f898a826c0812a8c412c95c30123a99b3e19b33
Andrey_Beloboky <andrei.beloboky@yandex.ru>
2026-05-22 15:44:24 +0300

    update path to delete cookie

--- src/main/java/org/example/filter/CorsFilter.java
+++ src/main/java/org/example/filter/CorsFilter.java
@@ -16,12 +16,13 @@ public class CorsFilter implements Filter {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-        httpResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, content-type");
-        httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
-
         if ("OPTIONS".equalsIgnoreCase(httpRequest.getMethod())) {
+
+            httpResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+            httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, content-type");
+            httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+
             httpResponse.setStatus(HttpServletResponse.SC_OK);
             return;
         }
